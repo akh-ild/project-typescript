72784ec1c216435f2ddd98835f3b92f7f7c3aae6
akh-ild <ildar.akhmadeyev@gmail.com>
2025-02-24 20:33:52 +0500

    interface, setup

--- 02-basic/interface.ts
+++ 02-basic/interface.ts
@@ -4,18 +4,27 @@ interface User {
   userId: number,
   goggleId?: string,
   startTrial: () => string,
-  getCoupon: (name: string) => number,
+  getCoupon: (name: string, value: number) => number,
+};
+
+interface User {
+  githubToken: string, // add property to User
+};
+
+interface Admin extends User {
+  role: 'admin' | 'ta' | 'learner';
 };
 
 const ildar: User = {
   id: 32,
   email: 'ildar@test.ts',
   userId: 2211,
+  githubToken: 'github',
   startTrial: () => {
     // return 1; // error
     return 'trail started';
   },
-  getCoupon: (name: string) => {
+  getCoupon: (name: string, value: number) => {
     return 10;
   }
 };
