d292273d8ce09584e20b429245e7466cd3016332
akh-ild <ildar.akhmadeyev@gmail.com>
2025-02-24 20:55:48 +0500

    class private value

--- 03-ts/dist/index.js
+++ 03-ts/dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 class User {
     constructor(email, name) {
-        this.city = '';
+        this.city = "Bishkek";
         this.email = email;
         this.name = name;
     }
 }
+class User1 {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Bishkek";
+    }
+}
 const ildar = new User('ildar@test.ts', 'Ildar');
-ildar.city = 'Bishkek';
--- 03-ts/src/index.ts
+++ 03-ts/src/index.ts
@@ -1,12 +1,20 @@
 class User {
   email: string
   name: string
-  city: string = ''
+  private readonly city: string = "Bishkek"
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
   }
 }
 
+class User1 {
+  readonly city: string = "Bishkek";
+  constructor(
+    public email: string,
+    public name: string
+  ) {
+  }
+}
+
 const ildar = new User('ildar@test.ts', 'Ildar');
-ildar.city = 'Bishkek';
