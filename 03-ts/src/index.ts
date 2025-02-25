4799ef3cf8b16f98a2e912a77b5af9e01702b61f
akh-ild <ildar.akhmadeyev@gmail.com>
2025-02-25 17:04:42 +0500

    setter, getter

--- 03-ts/src/index.ts
+++ 03-ts/src/index.ts
@@ -1,10 +1,23 @@
 class User {
   email: string
   name: string
+  private _courseCount = 1
   private readonly city: string = "Bishkek"
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
+  }
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+  get courseCount(): number {
+    return this._courseCount;
+  }
+  set courseCount(courseNum):voidw { // setter cannot have return type
+    if (courseNum <= 1) {
+      throw new Error('Course count should be more than 1');
+    }
+    this._courseCount = courseNum;
   }
 }
 
