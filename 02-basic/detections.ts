9bc34d4f46831831283fdf194776cb1cbe5e3922
akh-ild <ildar.akhmadeyev@gmail.com>
2025-02-27 10:58:35 +0500

    as, instanceof operator narrowing

--- 02-basic/detections.ts
+++ 02-basic/detections.ts
@@ -15,3 +15,18 @@ function isAdmin(account: User | Admin) {
   }
   return false;
 }
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird) {
+  return (pet as Fish).swim !== undefined;
+}
